19eafdf3a2d63a0b62f771167130f3782cbc9d90
Shuva_Biswas <shuvabiswas12@gmail.com>
2021-12-29 21:38:09 +0600

    loginview done.

---
+++ AppoinmentScheduling/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using AppoinmentScheduling.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppoinmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppoinmentScheduling/Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppoinmentScheduling.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        [Display(Name = "Remember Me?")]
+        public bool RememberMe { get; set; }
+    }
+}
