4acc6d850a6b9c76284c49b74f5c1a4ebc6d72e0
Shuva_Biswas <shuvabiswas12@gmail.com>
2022-01-12 22:44:08 +0600

    added duration for viewbag.

--- AppoinmentScheduling/Utility/Helper.cs
+++ AppoinmentScheduling/Utility/Helper.cs
@@ -11,12 +11,26 @@ namespace AppointmentScheduling.Utility
 
         public static List<SelectListItem> GetRolesForDropDown()
         {
-            return new List<SelectListItem> 
-            { 
+            return new List<SelectListItem>
+            {
                 new SelectListItem{Value= Helper.Admin, Text=Helper.Admin },
                 new SelectListItem{Value= Helper.Doctor, Text=Helper.Doctor },
                 new SelectListItem{Value= Helper.Patient, Text=Helper.Patient }
             };
+        }
+
+        public static List<SelectListItem> GetTimeDropDown()
+        {
+            int minute = 60;
+            List<SelectListItem> duration = new List<SelectListItem>();
+            for (int i = 0; i < 12; i++)
+            {
+                duration.Add(new SelectListItem { Value = minute.ToString(), Text = i + " Hr" });
+                minute = minute + 30;
+                duration.Add(new SelectListItem { Value = minute.ToString(), Text = i + " Hr 30 min" });
+                minute = minute + 30;
+            }
+            return duration;
         }
     }
 }
